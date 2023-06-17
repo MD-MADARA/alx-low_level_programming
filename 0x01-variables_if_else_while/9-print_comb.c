ed15dd08d1165a8489578e49421e1986c666f45f
MD-MADARA <mouadotaku9421@gmail.com>
2023-06-17 01:39:39 +0100

    fix code 9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -11,12 +11,13 @@ int main(void)
 
 	while (num <= 9)
 	{
-		putchar('0' + num++);
+		putchar('0' + num);
 		if (num != 9)
 		{
 			putchar(',');
 			putchar(' ');
 		}
+        num++;
 	}
 	putchar('\n');
 	return (0);
