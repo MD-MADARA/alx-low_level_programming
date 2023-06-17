2145aee307179b666107ccf4c903eb78032c9ef7
MD-MADARA <mouadotaku9421@gmail.com>
2023-06-17 02:26:54 +0100

    102-print_comb5.c

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+*/
+int main(void)
+{
+	int num1, num2;
+
+	num1 = 0;
+	while (num1 <= 99)
+	{
+		num2 = 0;
+		while (num2 <= 99)
+		{
+			if (num1 < num2)
+			{
+				putchar('0' + num1 % 10);
+				putchar('0' + num1 / 10);
+				putchar(' ');
+				putchar('0' + num2 % 10);
+				putchar('0' + num2 / 10);
+				if (num1 != 98 || num2 != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+			num2++;
+		}
+		num1++;
+	}
+	putchar('\n');
+	return (0);
+}
