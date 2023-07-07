feebe829d4b994b2aa5ad9ae12ea223f9feadc77
MD-MADARA <mouadotaku9421@gmail.com>
2023-07-08 00:59:32 +0100

    multiplies two numbers

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+* main - Entry point
+*
+* @argc: Length of @argv
+* @argv: Array of strings of the arguments of this program
+*
+* Return: 0, Success
+*/
+
+int main(int argc, char **argv)
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	printf("%d\n", atoi(argv[1]) * atoi(argv[2]));
+}
