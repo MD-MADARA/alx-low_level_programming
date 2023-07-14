94ccedfcc1803f76197ce0517b4c9a0a853ed4f3
MD-MADARA <mouadotaku9421@gmail.com>
2023-07-14 22:00:15 +0100

    advanced realloc

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,4 +1,3 @@
-#include <stdlib.h>
 #include "main.h"
 
 /**
@@ -11,41 +10,40 @@
 */
 
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
-
 {
-char *ptr1;
-char *old_ptr;
-unsigned int i;
+	char *realloced, *old_ptr;
+	unsigned int i;
 
-if (new_size == old_size)
+	if (new_size == 0 && ptr)
+	{
+		free(ptr);
+		return (NULL);
+	}
 
-return (ptr);
-if (new_size == 0 && ptr)
+	if (!ptr)
+	{
+		realloced = malloc(new_size);
+		if (!realloced)
+			return (NULL);
+		return (realloced);
+	}
+	if (new_size == old_size)
+		return (ptr);
 
-{
-free(ptr);
-return (NULL);
-}
-if (!ptr)
-
-return (malloc(new_size));
-ptr1 = malloc(new_size);
-if (!ptr1)
-
-return (NULL);
-old_ptr = ptr;
-if (new_size < old_size)
-
-{
-for (i = 0; i < new_size; i++)
-ptr1[i] = old_ptr[i];
-}
-if (new_size > old_size)
-
-{
-for (i = 0; i < old_size; i++)
-ptr1[i] = old_ptr[i];
-}
-free(ptr);
-return (ptr1);
+	realloced = malloc(new_size);
+	if (!realloced)
+		return (NULL);
+	old_ptr = ptr;
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			realloced[i] = old_ptr[i];
+	}
+	if (new_size < old_size)
+	{
+		for (i = 0; i < new_size; i++)
+			realloced[i] = old_ptr[i];
+	}
+	free(old_ptr);
+	return (realloced);
 }
