6755a0c0cdca6cc11e91c5fac117abc80d93f92b
MD-MADARA <mouadotaku9421@gmail.com>
2023-07-18 02:08:59 +0100

    fix first task

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,10 +8,17 @@
  */
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	if (d)
+	if (!d)
 	{
-		d->name = name;
-		d->age = age;
-		(*d).owner = owner;
+		d = malloc(sizeof(struct dog));
+		if (!d)
+		{
+			printf("Error malloc\n");
+			return;
+		}
 	}
+
+	d->name = name;
+	d->age = age;
+	(*d).owner = owner;
 }
