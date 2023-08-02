9ea8c176b4aa9e5dc706e35a2f1ddd9a7dd0f227
MD-MADARA <mouadotaku9421@gmail.com>
2023-08-02 02:47:38 +0100

    fix

--- 0x13-more_singly_linked_lists/101-print_listint_safe.c
+++ 0x13-more_singly_linked_lists/101-print_listint_safe.c
@@ -7,78 +7,31 @@
  */
 size_t print_listint_safe(const listint_t *head)
 {
-	int i = 0;
-	list_addr *l_address = NULL;
-	char *address;
+	int i, flag;
 
 	if (head == NULL)
 		return (0);
 
-	while (head)
+	for (i = flag = 0; head != NULL || flag; i++)
 	{
-		address = (void *)head;
-		printf("[%p] %d\n", address, head->n);
+		printf("[%p] %d\n", (void *) head, head->n);
 
-		if (search_list(l_address, address))
+		if (head <= head->next)
 		{
-			printf("-> [%p] %d\n", (void *) head, head->n);
+			flag = 1;
 			break;
 		}
-		add_node_end(&l_address, address);
 
 		head = head->next;
+		flag = 0;
+	}
+
+	if (flag)
+	{
+		head = head->next;
+		printf("-> [%p] %d\n", (void *) head, head->n);
 		i++;
 	}
+
 	return (i);
-}
-/**
- * search_list - a function that search for value in list
- * @head: the head of the list
- * @value: the value to search
- *
- * Return: 1 if success or 0 if not
- */
-int search_list(list_addr *head, void *value)
-{
-	list_addr *l = head;
-
-	while (l)
-	{
-		if (l->address == value)
-			return (1);
-		l = l->next;
-	}
-	return (0);
-}
-/**
- * add_node_end - add  a new node to the beggining of the listint_t.
- * @head: pointer to pointer *head, that point to the structure.
- * @n: element to add
- *
- * Return: the new node
- */
-
-list_addr *add_node_end(list_addr **head, void *n)
-{
-	list_addr *new_node = NULL;
-	list_addr *ptr = NULL;
-
-	new_node = (list_addr *)malloc(sizeof(list_addr));
-	if (!new_node)
-		exit(98);
-	new_node->address = n;
-	new_node->next = NULL;
-	if (!*head)
-	{
-		new_node->next = *head;
-		*head = new_node;
-	}
-	else
-	{
-		ptr = *head;
-		while (ptr->next)
-			ptr = ptr->next;
-		ptr->next = new_node;
-	}
-	return (new_node);
 }
--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -19,17 +19,6 @@ typedef struct listint_s
 	struct listint_s *next;
 } listint_t;
 
-/**
- * struct list_addr - singly linked list
- * @address: value
- * @next: next node
- * Description: singly linked list
- */
-typedef struct list_addr
-{
-	void *address;
-	struct list_addr *next;
-} list_addr;
 
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
@@ -46,7 +35,5 @@ listint_t *reverse_listint(listint_t **head);
 size_t print_listint_safe(const listint_t *head);
 size_t free_listint_safe(listint_t **h);
 listint_t *find_listint_loop(listint_t *head);
-list_addr *add_node_end(list_addr **head, void *n);
-int search_list(list_addr *head, void *value);
 
 #endif
